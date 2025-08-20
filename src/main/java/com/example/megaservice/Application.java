70565d225d8dc82b72cd762b624ade4a13c986b3
jagadish valluri <jagadishvalluri64@gmail.com>
2025-08-20 21:41:06 +0530

    tomcat issue fixed

--- src/main/java/com/example/megaservice/Application.java
+++ src/main/java/com/example/megaservice/Application.java
@@ -1,15 +1,24 @@
 package com.example.megaservice;
 
+import java.util.Arrays;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-@EnableScheduling
-@EnableCaching
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+  private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @Bean
+  ApplicationRunner printProfiles(Environment env) {
+    return args -> log.info("Active profiles: {}", Arrays.toString(env.getActiveProfiles()));
+  }
 }
